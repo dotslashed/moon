d1f9bac6a8d7031379d5ac13bea3a52ec973dd7a
dotslashed <73475291+dotslashed@users.noreply.github.com>
2021-07-29 20:56:31 +0530

    Update validity_check.sh

--- validity_check.sh
+++ validity_check.sh
@@ -1,6 +1,8 @@
 #Valid Registrar address checker for a list of assets.
 #subs.txt is the list of assets.
 
+
 #!/bin/bash
 
-cat subs.txt | while read domain do;do curl -sk "https://who.is/whois/$domain" | if  grep -q -A 5 Registrar: ; then echo $domain-----OK_OK; else echo FAILED_FAILED;fi;done
+cat subs.txt | while read domain do;do curl -sk "https://who.is/whois/$domain" | if grep -q -F -e '  Belastingdienst Centrum voor Infrastructuur en Exp' -e '  Laan van Westenenk 490' -e '  7334DS Apeldoorn'
+-e '  Netherlands'; then echo $domain-----OK_OK; else echo FAILED_FAILED;fi;done
